abfd0a0f2af900ab95ede00020d01913a4e542f7
gkaguirre <gkaguirre@me.com>
2016-10-01 19:17:05 -0400

    Getting there on the Fourier model
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- analysis/mriBFDM_DeriveFourierHRF.m
+++ analysis/mriBFDM_DeriveFourierHRF.m
@@ -1,33 +1,111 @@
-function [ packetCellArrayOut ] = mriBFDM_DeriveFourierHRF( packetCellArrayIn, secondsToModel, harmonicsToModel )
+function [kernelStruct] = mriBFDM_FitFourierBasis( thePacket, msecsToModel, deltaTmsecsToModel, frequenciesToModel )
 % function [ packetCellArrayOut ] = mriBFDM_DeriveFourierHRF( packetCellArrayIn )
 %
 %
+%  frequenciesToModel - The number of Fourier components to have in the
+%  model fit. This includes the zeroeth (DC) frequency component.
 
-% Get the number of sessions
+frequenciesToModel=20;
+msecsToModel=16000;
+deltaTmsecsToModel=500;
 
-nSessions=size(packetCellArrayIn,1);
-nRuns=size(packetCellArrayIn,2);
+% create the fourier set matrix
+clear fourierSetStructure
+clear attentionEventPacket
+fourierSetStructure.timebase = ...
+    linspace(0,msecsToModel-deltaTmsecsToModel,msecsToModel);
+componentIndex = 1;
+fourierSetStructure.values(1,:) = ...
+    fourierSetStructure.timebase*0+1; % Create DC component
+componentIndex = componentIndex+1;
 
-for ss=1:nSessions
-    
-    for rr=1:nRuns
-        thePacket=packetCellArrayIn{ss,rr};
-        if ~isempty(thePacket)
-attentionEventPacket.stimulus.timebase=thePacket.stimulus.timebase;
+% loop through the harmonics
+for i = 1:ceil(frequenciesToModel/2)
+    % Create sine for the harmonic
+    fourierSetStructure.values(componentIndex,:) = ...
+        sin(fourierSetStructure.timebase/msecsToModel*2*pi*i);
+    componentIndex = componentIndex+1;
+    % Create cosine for the harmonic
+    fourierSetStructure.values(componentIndex,:) = ...
+        cos(fourierSetStructure.timebase/msecsToModel*2*pi*i);
+    componentIndex = componentIndex+1;
+end
+
+% trim the set down to the requested number of frequencies
+fourierSetStructure.values=fourierSetStructure.values(1:frequenciesToModel,:);
+
+% build the attentionEventPacket for fitting
 attentionEventPacket.response.timebase=thePacket.response.timebase;
-attentionEventPacket.response.values=thePacket.stimulus.timebase;
+attentionEventPacket.response.values=thePacket.response.values;
 attentionEventPacket.kernel=[];
 attentionEventPacket.metaData=[];
 
+% the stimulus values are built first as the vector of attention events
+attentionEventPacket.stimulus.timebase=thePacket.stimulus.timebase;
 impulseAttentionEvents=zeros(1,length(thePacket.stimulus.timebase));
 impulseAttentionEvents(thePacket.stimulus.metaData.eventTimesArray)=1;
+attentionEventPacket.stimulus.values = ...
+    repmat(impulseAttentionEvents,frequenciesToModel,1);
 
+% convolve the rows of the attentionEventPacket.stimulus.values by each of
+% the rows of the fourierSet
+for ii=1:frequenciesToModel
+    % convolve
+    valuesRowConv = conv(attentionEventPacket.stimulus.values(ii,:), ...
+        fourierSetStructure.values(ii,:),'full');
+    % cut off extra conv values
+    attentionEventPacket.stimulus.values(ii,:) = ...
+        valuesRowConv(1:length(attentionEventPacket.stimulus.timebase));  
+end % loop through rows of the Fourier Set
 
-attentionEventStruct.timebase=thePacket.stimulus.timebase;
-        end % thePacket is not empty        
-    end % loop over runs
-    
-end % loop over sessions
+% mean center the component that models the zeroeth frequency events.
+attentionEventPacket.stimulus.values(1,:) = ...
+    attentionEventPacket.stimulus.values(1,:) - ...
+    mean(attentionEventPacket.stimulus.values(1,:));
+
+% instantiate a model object that will be used for fitting
+temporalFit = tfeIAMP('verbosity','none');
+
+% set up the default properties of the fit
+paramLockMatrix = []; % unused
+defaultParamsInfo.nInstances = frequenciesToModel;
+
+% VALIDATION
+% copy and downsample one of the Fourier components into the
+%  values field. See if it can recover it.
+
+attentionEventPacket.response.values=rand(1,336)*0;
+resampledTimeSeries = ...
+    resample(timeseries(attentionEventPacket.stimulus.values(2,:),attentionEventPacket.stimulus.timebase),...
+    attentionEventPacket.response.timebase);
+attentionEventPacket.response.values=attentionEventPacket.response.values+...
+squeeze(resampledTimeSeries.Data)';
+resampledTimeSeries = ...
+    resample(timeseries(attentionEventPacket.stimulus.values(1,:),attentionEventPacket.stimulus.timebase),...
+    attentionEventPacket.response.timebase);
+attentionEventPacket.response.values=attentionEventPacket.response.values+...
+squeeze(resampledTimeSeries.Data)'*.5;
+attentionEventPacket.response.values=attentionEventPacket.response.values-...
+    mean(attentionEventPacket.response.values);
+
+% Derive the Fourier set fit
+[paramsFit,fVal,modelResponseStruct] = ...
+            temporalFit.fitResponse(attentionEventPacket,...
+            'defaultParamsInfo', defaultParamsInfo, ...
+            'paramLockMatrix',paramLockMatrix, ...
+            'searchMethod','linearRegression');
+
+% Recover the modeled response and place into a kernel struct.
+kernelStruct.timebase = ...
+    linspace(0,msecsToModel-deltaTmsecsToModel,msecsToModel/deltaTmsecsToModel);
+
+% Resample the fourierSetStructure to the timebase of the kernelStruct
+resampledFourierSetStructure = ...
+    temporalFit.resampleTimebase(fourierSetStructure,kernelStruct.timebase);
+
+% Multiply the amplitude values of the parameter fit by the elements of the
+% resampled Fourier Set
+kernelStruct.values=resampledFourierSetStructure.values'*paramsFit.paramMainMatrix;
 
 end
 
--- loadFuncs/mriBFDM_MakeResponseStruct.m
+++ loadFuncs/mriBFDM_MakeResponseStruct.m
@@ -9,11 +9,11 @@ resp                    = load_nifti(makeResponseStructParams.responseFile);
 % create the timebase
 TR                      = resp.pixdim(5)/1000;
 runDur                  = size(resp.vol,4);
-timebase = 0:TR:(runDur*TR)-TR;
+timebase = (0:TR:(runDur*TR)-TR)*1000;
 
 % load the region of interest
 roi                     = load_nifti(makeResponseStructParams.areasFile);
-regionIndicies                   = find(abs(roi.vol)==makeResponseStructParams.areasIndex);
+regionIndicies          = find(abs(roi.vol)==makeResponseStructParams.areasIndex);
 volDims                 = size(resp.vol);
 flatVol                 = reshape(resp.vol,volDims(1)*volDims(2)*volDims(3),volDims(4));
 
@@ -35,3 +35,4 @@ metaData.modulationDirection=makeResponseStructParams.modulationDirection;
 metaData.blockOrder=makeResponseStructParams.blockOrder;
 metaData.sessionObserver=makeResponseStructParams.sessionObserver;
 metaData.sessionDate=makeResponseStructParams.sessionDate;
+metaData.TRmsecs=TR*1000;
