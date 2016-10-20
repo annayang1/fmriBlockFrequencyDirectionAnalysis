19ff2c56fecef4ae4b5c5a8ebaeca48c814c0b49
gkaguirre <gkaguirre@me.com>
2016-10-20 12:26:48 -0400

    Adjusted the plot to have a zero % change value for zero freq stim
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- analysis/fmriBDFM_AnalyzeCarryOverEffects.m
+++ analysis/fmriBDFM_AnalyzeCarryOverEffects.m
@@ -9,6 +9,7 @@ nOrders=size(fitResultsStructAvgResponseCellArray,3);
 % Define these constants. Probably should have the first three pulled from
 % the passed cell array
 modDirections={'LightFlux','L-M','S'};
+clims=[-0.2,1;-0.2,0.6;0,0.4];
 stimOrders={'A','B'};
 theFrequencies=[0,2,4,8,16,32,64];
 colorStr = 'krb';
@@ -16,9 +17,15 @@ colorStr = 'krb';
 figure
 for ss=1:nSubjects
     for ii=1:nDirections
-        responseMatrix=mriBDFM_MakeCarryOverMatrix( fitResultsStructAvgResponseCellArray(ss,ii,:) );
+        responseMatrix=fmriBDFM_MakeCarryOverMatrix( fitResultsStructAvgResponseCellArray(ss,ii,:) );
+        
+        % Adjust the data to have a mean response of zero to zero frequency
+        % stimuli 
+        zeroFrequencyValue=mean(responseMatrix(:,1));
+        responseMatrix=responseMatrix-zeroFrequencyValue;
+        
         subplot(nSubjects,nDirections,ii+(ss-1)*nDirections);
-        imagesc(responseMatrix);
+        imagesc(responseMatrix,clims(ii,:));
         title(modDirections(ii)); set(gca,'xticklabel',([0 2 4 8 16 32 64]));
         set(gca,'yticklabel',([0 2 4 8 16 32 64])); xlabel('Current stimulus');
         ylabel('Prior stimulus'); set(gca,'FontSize',15); colorbar;
