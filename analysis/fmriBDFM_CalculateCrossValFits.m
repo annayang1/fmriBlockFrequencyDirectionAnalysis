55bf767cb2291101386764d3dd0b47b820229989
gkaguirre <gkaguirre@me.com>
2016-10-30 16:56:13 -0400

    This routine works
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- analysis/fmriBDFM_CalculateCrossValFits.m
+++ analysis/fmriBDFM_CalculateCrossValFits.m
@@ -1,44 +1,102 @@
-function [fitResultsStructAvgResponseCellArray] = fmriBDFM_CalculateCrossValFits(packetCellArray, hrfKernelStructCellArray)
+function [ xValFitStructureCellArray ] = fmriBDFM_CalculateCrossValFits(packetCellArray, hrfKernelStructCellArray)
 % function [packetCellArray] = fmriBDFM_CalculateCrossValFits(thePacket, hrfKernelStructCellArray)
 %
 
+% Announce our intentions
+fprintf('>> Calculating cross-validated parameters\n');
+
+% Construct the model object
+tfeHandle = tfeIAMP('verbosity','none');
+
+% How many partitions will we test in the cross validation?
 maxPartitions=100;
 
-% Build some arrays to identify the stimulus types in each packet
+fprintf(['\t Will examine ' strtrim(num2str(maxPartitions)) ' parititions for each subject / direction.\n']);
+
+%% Prepare the packetCellArray for fitting
+% Loop through the packets and
+%  - prepare the HRF kernel
+%  - downsample the stimulus array for speed
+%  - build an array to identify the stimulus type in each packet
+
+fprintf('\t Preparing the HRFs and stimulus vectors\n');
+
 nSubjects=size(packetCellArray,1);
 nRuns=size(packetCellArray,2);
+modDirections={'LightFlux','L-M','S'};
 for ss=1:nSubjects
+
+    % Grab the average hrf and prepare it as a kernel
+    % Assume the deltaT of the response timebase is the same
+    % across packets for this subject
+    theHRFKernelStruct=hrfKernelStructCellArray{ss};
+    check = diff(packetCellArray{ss,1}.response.timebase);
+    responseDeltaT = check(1);
+    nSamples = ceil((theHRFKernelStruct.timebase(end)-theHRFKernelStruct.timebase(1))/responseDeltaT);
+    newKernelTimebase = theHRFKernelStruct.timebase(1):responseDeltaT:(theHRFKernelStruct.timebase(1)+nSamples*responseDeltaT);
+    theHRFKernelStruct = tfeHandle.resampleTimebase(theHRFKernelStruct,newKernelTimebase);
+    theHRFKernelStruct = prepareHRFKernel(theHRFKernelStruct);
+
     for rr=1:nRuns
-        if ~isempty(packetCellArray{ss,rr})
-            modulationDirectionCellArray{ss,rr}=(packetCellArray{ss,rr}.stimulus.metaData.modulationDirection);
+        thePacket=packetCellArray{ss,rr};
+        if ~isempty(thePacket)
+            % Build the list of modulationDirections
+            modulationDirectionCellArray{ss,rr}=(thePacket.stimulus.metaData.modulationDirection);
+
+            % Place the kernel struct in the packet
+            thePacket.kernel = theHRFKernelStruct;
+                        
+            % downsample the stimulus values to 100 ms deltaT to speed things up
+            totalResponseDuration=thePacket.response.metaData.TRmsecs * ...
+                length(thePacket.response.values);
+            newStimulusTimebase=linspace(0,totalResponseDuration-100,totalResponseDuration/100);
+            thePacket.stimulus=tfeHandle.resampleTimebase(thePacket.stimulus,newStimulusTimebase);
+            
+            % put the modified packet back into the cell arrray
+            packetCellArray{ss,rr}=thePacket;
+            
         end % the packet is not empty
     end % loop over runs
 end % loop over subjects
 
-
-modDirections={'LightFlux','L-M','S'};
-
-fprintf('>> XXX\n');
+fprintf('\t Looping over subjects and modulation directions\n');
 
 for ss=1:nSubjects
     for ii=1:length(modDirections)
-        
+
+        fprintf('\t\t * Subject <strong>%g</strong> , modDirection <strong>%g</strong>\n', ss, ii);
+
         % Identify the set of packets with this modulation direction for
         % this subject
         theCellIndices=find( strcmp(modulationDirectionCellArray(ss,:),modDirections{ii})==1 );
-        subPacketCellArray=packetCellArray(theCellIndices);
+        subPacketCellArray=packetCellArray(ss,theCellIndices);
         
         % Build a linked partition matrix for this set of packets
-        partitionMatrix = fmriBDFM_CreateLinkedPartitionMatrix(subPacketCellArray);
+        partitionMatrix = fmriBDFM_CreateLinkedPartitionMatrix(subPacketCellArray);        
+        nPartitions=size(partitionMatrix,1);        
+
+        % Take a random subset of all available partitions
+        ix=randperm(nPartitions);
+        partitionMatrix=partitionMatrix(ix,:);
+        partitionMatrix=partitionMatrix(1:maxPartitions,:);
+        nPartitions=size(partitionMatrix,1);
         
-        % 
+        % Conduct the cross validation
+        [ xValFitStructure ] = crossValidateFits( subPacketCellArray, tfeHandle, ...
+            'partitionMatrix', partitionMatrix, ...
+            'aggregateMethod', 'mean',...
+            'verbosity', 'none',...
+            'searchMethod','linearRegression', ...
+            'errorType','1-r2');
         
-        
+        xValFitStructureCellArray{ss,ii}=xValFitStructure;
         
     end % loop over modulation directions
 end % loop over subjects
 
 end % main function
+
+
 
 
 
@@ -138,11 +196,11 @@ for ii=1:nPartitions
         end
     end
     % Check that there is no row duplication
-%     for jj=1:nPartitions
-%         if isequal(partitionMatrix(ii,:),partitionMatrix(jj,:)) && ii~=jj
-%             error('Two rows of the partition matrix are the same');
-%         end
-%     end
+    %     for jj=1:nPartitions
+    %         if isequal(partitionMatrix(ii,:),partitionMatrix(jj,:)) && ii~=jj
+    %             error('Two rows of the partition matrix are the same');
+    %         end
+    %     end
 end
 
 end % sub-function
